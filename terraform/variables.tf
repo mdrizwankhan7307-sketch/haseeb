b5e0d690c133da3e527293183cd97d02862ade32
haseeb8801-dev <haseebahmed8801@gmail.com>
2026-07-03 12:33:41 +0000

    modified variable file

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,73 +1,49 @@
 variable "region" {
-
-  default = "us-east-1"
-
+  description = "AWS Region"
+  type        = string
+  default     = "us-east-1"
 }
 
 variable "vpc_cidr" {
-
-  default = "10.0.0.0/16"
-
+  description = "VPC CIDR"
+  type        = string
+  default     = "10.0.0.0/16"
 }
 
 variable "public_subnets" {
+  description = "Public Subnets"
+  type        = list(string)
 
   default = [
-
     "10.0.1.0/24",
-
-    "10.0.2.0/24",
-
-    "10.0.3.0/24"
-
+    "10.0.2.0/24"
   ]
-
 }
 
 variable "private_subnets" {
+  description = "Private Subnets"
+  type        = list(string)
 
   default = [
-
-    "10.0.4.0/24",
-
-    "10.0.5.0/24",
-
-    "10.0.6.0/24"
-
+    "10.0.3.0/24",
+    "10.0.4.0/24"
   ]
-
 }
 
-#############################################
-# RDS Variables
-#############################################
-
 variable "db_username" {
-
-  description = "RDS Username"
-
-  type = string
-
-  default = "admin"
-
+  description = "Database Username"
+  type        = string
+  default     = "admin"
 }
 
 variable "db_password" {
-
-  description = "RDS Password"
-
-  type = string
-
-  sensitive = true
-
+  description = "Database Password"
+  type        = string
+  sensitive   = true
 }
 
 variable "db_name" {
-
   description = "Database Name"
-
-  type = string
-
-  default = "studentdb"
-
+  type        = string
+  default     = "studentdb"
 }
